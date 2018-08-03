a531923b68578b7c65938506c368e1b7709875ff
Dani Lyn <dlyn1110@gmail.com>
2018-08-03 01:56:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,82 +8,41 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
------Create
-  get '/posts/new' do
+get '/posts/new' do
     erb :new
   end
 
-  post '/posts' do #<--creates a post --
-    @post = Post.create(name: params[:name], content: params[:content])
-    redirect to '/posts'
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content])
+    erb :index
   end
-  #----Read
+
   get '/posts' do
     @posts = Post.all
     erb :index
   end
 
   get '/posts/:id' do
-    @post = Post.find(params[:id])
+    @post = Post.find_by id: params[:id]
     erb :show
   end
 
-  #---Update
   get '/posts/:id/edit' do
-    @post = Post.find(params[:id])
+    @post = Post.find_by id: params[:id]
+
     erb :edit
   end
 
   patch '/posts/:id' do
-    @post = Post.find(params[:id])
-    @post = Post.update(name: params[:name], content: params[:content])
+    @post = Post.find_by id: params[:id]
+    @post.update(name: params[:name], content: params[:content])
+    @post.save
     erb :show
   end
 
-#-----------DELETE
   delete '/posts/:id/delete' do
-    @post = Post.find(params[:id])
-    @post.destroy
+    @post = Post.find_by id: params[:id]
+    @post.delete
     erb :delete
   end
 end
-
-
-# get '/posts/new' do
-#     erb :new
-#   end
-#
-#   post '/posts' do
-#     Post.create(name: params[:name], content: params[:content])
-#     erb :index
-#   end
-#
-#   get '/posts' do
-#     @posts = Post.all
-#     erb :index
-#   end
-#
-#   get '/posts/:id' do
-#     @post = Post.find_by id: params[:id]
-#     erb :show
-#   end
-#
-#   get '/posts/:id/edit' do
-#     @post = Post.find_by id: params[:id]
-#
-#     erb :edit
-#   end
-#
-#   patch '/posts/:id' do
-#     @post = Post.find_by id: params[:id]
-#     @post.update(name: params[:name], content: params[:content])
-#     @post.save
-#     erb :show
-#   end
-#
-#   delete '/posts/:id/delete' do
-#     @post = Post.find_by id: params[:id]
-#     @post.delete
-#     erb :delete
-#   end
-# end
