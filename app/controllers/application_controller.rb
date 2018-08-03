a5f5d5dcea642ac218f0e82f257ea65e7cfbe4cb
Dani Lyn <dlyn1110@gmail.com>
2018-08-03 01:52:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,41 +49,41 @@ class ApplicationController < Sinatra::Base
 # end
 
 
-get '/posts/new' do
-    erb :new
-  end
-
-  post '/posts' do
-    Post.create(name: params[:name], content: params[:content])
-    erb :index
-  end
-
-  get '/posts' do
-    @posts = Post.all
-    erb :index
-  end
-
-  get '/posts/:id' do
-    @post = Post.find_by id: params[:id]
-    erb :show
-  end
-
-  get '/posts/:id/edit' do
-    @post = Post.find_by id: params[:id]
-
-    erb :edit
-  end
-
-  patch '/posts/:id' do
-    @post = Post.find_by id: params[:id]
-    @post.update(name: params[:name], content: params[:content])
-    @post.save
-    erb :show
-  end
-
-  delete '/posts/:id/delete' do
-    @post = Post.find_by id: params[:id]
-    @post.delete
-    erb :delete
-  end
-end
+# get '/posts/new' do
+#     erb :new
+#   end
+# 
+#   post '/posts' do
+#     Post.create(name: params[:name], content: params[:content])
+#     erb :index
+#   end
+# 
+#   get '/posts' do
+#     @posts = Post.all
+#     erb :index
+#   end
+# 
+#   get '/posts/:id' do
+#     @post = Post.find_by id: params[:id]
+#     erb :show
+#   end
+# 
+#   get '/posts/:id/edit' do
+#     @post = Post.find_by id: params[:id]
+# 
+#     erb :edit
+#   end
+# 
+#   patch '/posts/:id' do
+#     @post = Post.find_by id: params[:id]
+#     @post.update(name: params[:name], content: params[:content])
+#     @post.save
+#     erb :show
+#   end
+# 
+#   delete '/posts/:id/delete' do
+#     @post = Post.find_by id: params[:id]
+#     @post.delete
+#     erb :delete
+#   end
+# end
